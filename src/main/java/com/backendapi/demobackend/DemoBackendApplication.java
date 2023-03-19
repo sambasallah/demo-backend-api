300ac1362b4baa85ac891164026a4caa793be78a
Samba Sallah <sambasallah10@gmail.com>
2023-03-18 22:31:46 -0500

    added github workflow

--- src/main/java/com/backendapi/demobackend/DemoBackendApplication.java
+++ src/main/java/com/backendapi/demobackend/DemoBackendApplication.java
@@ -2,12 +2,28 @@ package com.backendapi.demobackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DemoBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoBackendApplication.class, args);
 	}
+
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello World!";
+	}
+
+	@GetMapping("/is-alive")
+	public String isAlive() {
+		return "Yes";
+	}
+
+
 
 }
